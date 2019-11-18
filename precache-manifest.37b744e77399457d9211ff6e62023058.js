b3fa282192fa6ac5a4c251f1f2d79f3350cdfa19
shubham chavan <stchavan.17@gmail.com>
2019-11-18 12:36:08 +0530

    Updates

---
+++ precache-manifest.37b744e77399457d9211ff6e62023058.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c44bb07c2cf981435d47c678f2a145",
+    "url": "/recipe-app-react/index.html"
+  },
+  {
+    "revision": "f777d2f46d66ee1887ae",
+    "url": "/recipe-app-react/static/css/main.d05c0cc6.chunk.css"
+  },
+  {
+    "revision": "8dae4193fdc06cc6f658",
+    "url": "/recipe-app-react/static/js/2.95de4d4f.chunk.js"
+  },
+  {
+    "revision": "f777d2f46d66ee1887ae",
+    "url": "/recipe-app-react/static/js/main.81a0efca.chunk.js"
+  },
+  {
+    "revision": "3c49e026a444709a4bc4",
+    "url": "/recipe-app-react/static/js/runtime-main.d9a76fd6.js"
+  }
+]);
